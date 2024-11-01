fb307bdd2b503875a91336085ba9ad001889af6a
Aman Kathait <aman.kathat011@gmail.com>
2024-11-01 19:10:48 +0530

    7 Airbnb

--- NodeJs and ExpressJS/7 Airbnb/app.js
+++ NodeJs and ExpressJS/7 Airbnb/app.js
@@ -3,28 +3,20 @@ const path=require('path');
 const express=require('express');
 const bodyParser=require('body-parser');
 
-const hostRouter=require("./hostRouter");
-const storeRouter=require("./storeRouter");
+const hostRouter=require("./routers/hostRouter");
+const storeRouter=require("./routers/storeRouter");
+const rootDir=require('./util/path-util');
 
 const app=express();
+app.use(express.static(path.join(rootDir,"public")))
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(storeRouter);
 app.use("/host",hostRouter);
 
 app.use((req,res,next)=>{
   res.statusCode=404;
-  res.write(`
-    <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <title>Page Not Found</title>
-      </head>
-      <body>
-        <h1>404 Page Not Found</h1>
-      </body>
-      </html>
-    `);
-    res.end();
+  res.sendFile(path.join(rootDir,"views","404.html"));
+  
 })
 
 const PORT = 3000;
--- NodeJs and ExpressJS/7 Airbnb/storeRouter.js
+++
@@ -1,9 +0,0 @@
-const path=require('path');
-const express=require('express');
-//const rootDir=require("./util/path-util");
-const storeRouter=express.Router();
-
-storeRouter.get("/",(req,res,next)=>{
-  res.sendFile(path.join(__dirname,"views","index.html")); 
-});
-module.exports=storeRouter;
