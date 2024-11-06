7fb7f350b82742aece3d2c4d019610488fdccb25
Aman Kathait <aman.kathat011@gmail.com>
2024-11-06 20:38:34 +0530

    editing flag send successfully

--- NodeJs and ExpressJS/10 Dynamic Path/controllers/hostController.js
+++ NodeJs and ExpressJS/10 Dynamic Path/controllers/hostController.js
@@ -1,7 +1,30 @@
 const Home = require('./../models/Home');
 
 exports.getAddHome = (req, res, next) => {
-  res.render("host/add-home", {pageTitle: 'Host Your Home'});
+  res.render("host/edit-home", {pageTitle: 'Host Your Home'});
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId=req.params.homeId;
+  const editing=req.query.editing==='true';
+  if (!editing){
+    console.log("Editing flag not set properly");
+    return res.redirect("/host/host-homes");
+  }
+  Home.findById(homeId,home=>{
+    if (!home) {
+      console.log("Home not found for editing");
+      return res.redirect("/host/host-homes");
+    }
+    console.log(homeId,editing,home);
+    res.render("host/edit-home", {pageTitle: 'Edit Your Home'});
+  })
+};
+
+exports.getHostHomes = (req, res, next) => {
+  Home.fetchAll(registeredHomes => {
+    res.render("host/host-homes", { homes: registeredHomes, pageTitle: "Tumahara airbnb" });
+  });
 };
 
 exports.postAddHome = (req, res, next) => {
--- NodeJs and ExpressJS/10 Dynamic Path/routers/hostRouter.js
+++ NodeJs and ExpressJS/10 Dynamic Path/routers/hostRouter.js
@@ -5,4 +5,6 @@ const hostController = require('./../controllers/hostController');
 hostRouter.get("/add-home", hostController.getAddHome);
 hostRouter.post("/add-home", hostController.postAddHome);
 
+hostRouter.get("/host-homes", hostController.getHostHomes);
+hostRouter.get("/edit-home/:homeId",hostController.getEditHome);
 exports.hostRouter = hostRouter;
