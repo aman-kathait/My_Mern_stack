63148669648c42ac191f4f39dc6f442bf05942da
Aman Kathait <aman.kathat011@gmail.com>
2024-11-17 18:42:31 +0530

    Favourite model in mongoose

--- NodeJs and ExpressJS/13 mongoose/controllers/storeController.js
+++ NodeJs and ExpressJS/13 mongoose/controllers/storeController.js
@@ -17,20 +17,17 @@ exports.getHomes = (req, res, next) => {
 };
 
 exports.getFavourites = (req, res, next) => {
-  Favourite.fetchAll().then(favouriteIds => {
-    Home.find().then(registeredHomes=>{
-      favouriteIds=favouriteIds.map(favId=>favId.homeId);
-      console.log(favouriteIds,registeredHomes);
-      const favouriteHomes = registeredHomes.filter(home => favouriteIds.includes(home._id.toString()));
-      res.render("store/favourites", { homes: favouriteHomes, pageTitle: "Favourites" });
-    });
-  })
+  Favourite.find().populate("homeId").then((favIdHomes) => {
+      const favouriteHomes=favIdHomes.map((favIdHome)=>favIdHome.homeId);
+      res.render("store/favourites", { homes: favouriteHomes, pageTitle: "Favourites" ,
 
+      });
+    });
 };
 
 exports.postAddFavourites = (req, res, next) => {
   const homeId = req.body.id;
-  const fav=new Favourite(homeId);
+  const fav=new Favourite({homeId});
   fav.save().then(()=>{
     res.redirect("/favourites");
   }).catch(err=>{
@@ -51,7 +48,7 @@ exports.getHomeDetails = (req, res, next) => {
 }
 exports.postRemoveFavourite=(req,res,next)=>{
   const homeId=req.params.homeId;
-  Favourite.deleteById(homeId).then(()=>{
+  Favourite.findOneAndDelete({homeId}).then(()=>{
     res.redirect("/favourites");
   }).catch((error)=>{
     console.log('Error while remove from favourites',error);
--- NodeJs and ExpressJS/13 mongoose/models/Favourite.js
+++ NodeJs and ExpressJS/13 mongoose/models/Favourite.js
@@ -1,25 +1,12 @@
+const mongoose=require('mongoose');
 
-module.exports = class Favourite {
-  constructor(homeId){
-    this.homeId=homeId;
-  }
-  save() {
-    const db=getDb();
-    return db.collection("favourites").findOne
-    ({homeId:this.homeId}).then(existingFav=>{
-      if (!existingFav) {
-        return db.collection("favourites").insertOne(this);
-      }
-      return Promise.resolve();
-    })
-  }
-  static fetchAll() {
-    const db=getDb();
-    return db.collection('favourites').find().toArray()
-  }
+const favouriteSchema=new mongoose.Schema({
+  homeId: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Home' ,
+    required:true,
+    unique:true,
+  },
+});
 
-  static deleteById(homeId) {
-    const db=getDb();
-    return db.collection('favourites').deleteOne({homeId});
-  }
-}
+module.exports=mongoose.model("Favourite",favouriteSchema);
